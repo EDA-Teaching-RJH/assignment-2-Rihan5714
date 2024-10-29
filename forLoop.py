20f55f47d9ae10342599d3cf87a68782b8f78b92
Rihan5714 <rm858@kent.ac.uk>
2024-10-29 00:10:39 +0000

    Update forLoop.py

--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,5 @@
-### Part One -- your code goes here. 
+# Loop through numbers from 1 to 10
+for number in range(1, 11):
+    square = number ** 2  # Calculate the square of the number
+    print(f"The square of {number} is {square}")
+
